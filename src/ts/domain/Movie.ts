e579d2ed9495ff49c5fd4313f458101d53f29696
cpb-home <cpb@bizb.ru>
2024-02-24 16:18:13 +0300

    second initial commit

--- src/ts/domain/Movie.ts
+++ src/ts/domain/Movie.ts
@@ -1,3 +1,11 @@
 export default class Movie {
-    
+  constructor(
+    rusTitle: string,
+    engTitle: string,
+    year: number,
+    country: string,
+    tagline: string,
+    genre: [],
+    time: number
+  ) { }
 }
